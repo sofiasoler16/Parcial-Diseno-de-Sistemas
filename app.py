2debd908b027f4d3dd6e4df89d763d98e7859799
s.soler <s.soler@alumno.um.edu.ar>
2024-11-04 20:37:43 -0300

    app

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 # app.py
+import os
 from controllers.mutant_controller import app
 from database.db_connection import initialize_database
 
+
 if __name__ == '__main__':
-    initialize_database()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port)
